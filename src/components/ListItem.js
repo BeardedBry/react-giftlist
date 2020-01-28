e84c0eca52fdf1a309f3d7e41b3312bb7b0dc263
Brian Burkard <brianburkard@gmail.com>
2020-01-28 06:29:31 -0500

    refactored conditional in listitem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,65 +4,74 @@ const ListItem = ({ data, modifyItem, removeItem }) => {
     //modifyItem = (id, property, value)
     //removeItem = (id)
 
-    // TODO: refactor so both states aren't seperate return statements.
-    // show different if field is saved or being edited.
-    if(data.state === 'saved'){ // SAVED state
-        return (
-            <div className="WishList-ListItem">
-                {data.name}
-                {data.cost}
-                {data.description}
-                <button onClick={()=>modifyItem(data.id, 'state', 'edit')}>
-                    Edit
-                </button>
-                <button onClick={()=>removeItem(data.id)}>Remove</button>
-            </div>
-        )
-    } else if(data.state === 'edit') { // EDIT state
-        return (
-            <form onSubmit={(e)=>{
-                e.preventDefault();
-                if(!e.target.name.value){
-                    // show error
-                    e.target.name.placeholder="Please enter a value";
-                    return;
-                }
-                modifyItem(data.id, 'state', 'saved');
-            }}>
-                <label>
-                    Name:
-                    <input
-                        type="text"
-                        name="name" // has to match state object id
-                        value={data.name}
-                        onChange={( {target} )=> {
-                            modifyItem(data.id, target.name, target.value);
-                            }
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        if(data.state === 'edit') {
+            if(!event.target.name.value){
+                // show error
+                event.target.name.placeholder="Please enter a value";
+                return;
+            }
+            modifyItem(data.id, 'state', 'saved');
+            return;
+        }else if(data.state === 'saved') {
+            modifyItem(data.id, 'state', 'edit')
+        }
+    }
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <label>
+                {
+                data.state === 'edit' ? (  <input
+                    type="text"
+                    name="name" // has to match state object id
+                    value={data.name}
+                    onChange={( {target} )=> {
+                        modifyItem(data.id, target.name, target.value);
                         }
-                    />
-                </label>
-                <label>
-                    Description:
-                    <input 
-                        value={data.description}
-                        name="description"
-                        type="textarea"
-                        onChange={( {target} )=> {
-                            modifyItem(data.id, target.name, target.value);
-                            }
-                        } 
-                    />
-                </label>
+                    }
+                />
+                ):(
+                    data.name
+                )
+                }
+            </label>
+            <label>
+                {
+                data.state === 'edit' ? (
+                <input 
+                    value={data.description}
+                    name="description"
+                    type="textarea"
+                    onChange={( {target} )=> {
+                        modifyItem(data.id, target.name, target.value);
+                        }
+                    } 
+                />):(
+                    data.description
+                    )
+                }
+            </label>
+            {
+            data.state === 'edit' ? (
                 <input 
                     type="submit" 
                     value="Save"
                 />
-                <button onClick={()=>removeItem(data.id)}>
-                    Remove
-                </button>
-            </form>
+            ):(
+                <input 
+                type="submit" 
+                value="Edit"
+            />
+            ) 
+            }
+            <button onClick={()=>removeItem(data.id)}>
+                Remove
+            </button>
+        </form>
         )
     }
-}
+
 
 export default ListItem;
